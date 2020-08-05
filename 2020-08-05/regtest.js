5748f2bc6957a449cc2ffc0e176b88bceba4a078
guozilin <728109508@qq.com>
2020-08-05 18:07:24 +0800

    正则测试 test

--- 2020-08-05/regtest.js
+++ 2020-08-05/regtest.js
@@ -5,3 +5,18 @@ t.forEach(item=>{
 	// console.log(timerReg.test(item))
 	console.log(item.match(timerReg))
 })
+
+var convertToTitle = function(n) {
+	const allWord = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+	let result = ''
+	while(n>0){
+		n--
+		result += allWord[(n%26)] 
+		n = Math.floor(n/26)
+	}
+	return result
+};
+let ns = [701,20,30,10000,900,26]
+ns.forEach(item=>{
+	console.log(convertToTitle(item))
+})
